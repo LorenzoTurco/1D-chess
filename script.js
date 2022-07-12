12b1cccbc5336dc62b85d1e84e9f5345aa1eb47c
LorenzoTurco <l.turco@se19.qmul.ac.uk>
2022-07-12 18:04:04 +0100

    + Added isChecked function

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let gameFinished = false;
 let legalMovesDisplayed = false;
 let legalMoves = [];
 let fromPosition = "";
+let isInCheck = false;
 
 //Who's turn
 let colorTurn = "white";
@@ -17,31 +18,37 @@ let colorTurn = "white";
 const whiteKing = {
   color: "white",
   move: 1,
+  name: "king",
 };
 
 const blackKing = {
   color: "black",
   move: 1,
+  name: "king",
 };
 
 //knigths
 const whiteKnight = {
   color: "white",
   move: 2,
+  name: "knight",
 };
 const blackKnight = {
   color: "black",
   move: 2,
+  name: "knight",
 };
 
 //roocks
 const whiteRook = {
   color: "white",
   move: -1,
+  name: "rook",
 };
 const blackRook = {
   color: "black",
   move: -1,
+  name: "rook",
 };
 
 const chessBoard = {
@@ -63,15 +70,10 @@ const chessBoard = {
 }
 */
 
-const makeMove = (event) => {
-  //press on brown color not the pointer
-  let toPosition = Number(event.target.getAttribute("value"));
-
-  if (legalMoves.includes(toPosition)) {
-    updateBoardArray(fromPosition, toPosition);
-    updateIcons(fromPosition, toPosition);
-  }
-  changeTurn();
+const removePointers = () => {
+  Array.from(pointers).forEach((pointer) => {
+    pointer.style.display = "none";
+  });
 };
 
 const updateBoardArray = (fromPosition, toPosition) => {
@@ -89,11 +91,111 @@ const updateIcons = (fromPosition, toPosition) => {
 const changeTurn = () => {
   colorTurn == "black" ? (colorTurn = "white") : (colorTurn = "black");
   legalMovesDisplayed = false;
-  console.log(chessBoard);
+  legalMoves = [];
+};
+
+const makeMove = (event) => {
+  //press on brown color not the pointer
+  let toPosition = Number(event.target.getAttribute("value"));
+
+  if (legalMoves.includes(toPosition)) {
+    updateBoardArray(fromPosition, toPosition);
+    updateIcons(fromPosition, toPosition);
+
+    changeTurn();
+
+    if (isChecked()) {
+    }
+  } else {
+    console.log("move not allowed");
+    removePointers();
+    legalMoves = [];
+    legalMovesDisplayed = false;
+  }
+  return;
+};
+
+const isChecked = () => {
+  //CHECK IF YOUR KING IS IN CHECK
+  //Find your king
+  let kingPosition = 0;
+
+  for (let i = 0; i < chessBoard.board.length; i++) {
+    //find king position
+
+    if (
+      chessBoard.board[i].name == "king" &&
+      chessBoard.board[i].color == colorTurn
+    ) {
+      kingPosition = i;
+      break;
+    }
+  }
+
+  //find if opponent pieces are puttin your king in check
+  //Find all possible moves
+
+  for (let i = 0; i < chessBoard.board.length; i++) {
+    if (chessBoard.board[i] == "") continue;
+    if (chessBoard.board[i].color == colorTurn) continue; //ignore your own pieces
+
+    //ONLY ENEMY PIECES GET CHECCKED PAST HERE
+
+    switch (chessBoard.board[i].name) {
+      case "knight":
+        if (
+          i + chessBoard.board[i].move == kingPosition ||
+          i - chessBoard.board[i].move == kingPosition
+        ) {
+          isInCheck = true;
+          console.log("CHECK");
+
+          break;
+        }
+        break;
+
+      case "rook":
+        //check left side
+
+        for (let j = 1; j <= i; j++) {
+          if (chessBoard.board[i - j] == "") continue;
+
+          if (chessBoard.board[i - j].color == chessBoard.board[i].color) break;
+
+          if (i - j == kingPosition) {
+            //is in check
+            console.log("CHECK");
+
+            isInCheck = true;
+          }
+          break;
+        }
+
+        for (let j = i; j < chessBoard.board.length - 1; j++) {
+          if (chessBoard.board[j + 1] == "") continue;
+
+          if (chessBoard.board[j + 1].color == chessBoard.board[i].color) break;
+
+          if (j + 1 == kingPosition) {
+            //is in check
+            console.log("CHECK");
+            isInCheck = true;
+          }
+          break;
+        }
+        break;
+      case "king":
+        console.log("its a king");
+        break;
+    }
+  }
+
+  return isInCheck;
 };
 
 const showLegalMoves = (event) => {
   removePointers();
+
   if (legalMovesDisplayed) {
     makeMove(event);
     return;
@@ -180,12 +282,6 @@ const showLegalMoves = (event) => {
     }
   }
   return;
-};
-
-const removePointers = () => {
-  Array.from(pointers).forEach((pointer) => {
-    pointer.style.display = "none";
-  });
 };
 
 Array.from(tiles).forEach((tile) => {
